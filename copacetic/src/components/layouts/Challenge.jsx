35b441dd2b0c4c66f4ea5e8be404a54b1b5e791f
Ankit Mehta <ankitmmehta08@gmail.com>
2025-09-25 10:02:43 +0530

    word sync

--- copacetic/src/components/layouts/Challenge.jsx
+++ copacetic/src/components/layouts/Challenge.jsx
@@ -32,20 +32,51 @@ export default function Challenge(props) {
     return(
        <section id="challenge" >
             <h1>{word}</h1>
-            <p>{definition} </p>
+            {isNewWord && (<p>{definition} </p>)}
             <div className="helper" >
                 <div>
                 {/* CONTAINS ALL THE ERROR CORRECTION VISUAL BARS  */}
-                {[...Array(definition.length).keys()].map((element, elementIdx) => {
-                    // determine wether 
+                {[...Array(definition.length).keys()].map((char, elementIdx) => {
+                    // determine wether or not the user has tyoed the chareacter theythink is correct, and show red pr blur depending on whether or not it's actually correct
 
+                    const styleToApply= inputVal.length < char + 1 ? '' : 
+                    inputVal.split('')[elementIdx].toLowerCase() == definition.split('')[elementIdx].toLowerCase() ? 'correct' : 'incorrect'
+ 
                     return(
-                        <div key={elementIdx}></div>
+                        <div 
+                        className={'' + styleToApply }
+                        key={elementIdx}></div>
                     )
 
                 })}
                 </div>
-                <input type="text" placeholder="Enter the definition..." />
+                <input 
+                value={inputVal}
+                onChange={(e) => {
+//if a user has entered th correct num og char we need to do few thing 1: id the entry is correct we need to incr sttpemts and move them to next word 2: if the entery is oincorrect we need to increment attpemts and if they 
+
+if(e.target.value.length == definition.length && e.target.value > inputVal.length ) {
+    handleIncrementAttempts()
+//compare words
+if(e.target.value.toLowerCase() == definition.toLowerCase()) {
+//then the user correct outcome
+if(wordIndex >= listToLearn.length -1) {
+    handleCompleteDay()
+    return
+}
+setWordIndex(wordIndex + 1)
+setShowDefinition(false)
+setInputVal('')
+return
+
+//check if finished all the words, then end the day otherwise go to next word
+}
+
+}
+
+                    setInputVal(e.target.value)
+                }}
+                type="text" placeholder="Enter the definition..." />
             </div>
             <div className="challenge-btns">
                 <button className="card-button-secondary">
